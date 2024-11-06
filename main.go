75de0d86446b4699a0f1ac86d3bdefa99721f048
toni-akintola <oakintol@nd.edu>
2024-11-06 00:02:00 -0500

    working server

--- main.go
+++ main.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/alpacahq/alpaca-trade-api-go/v3/alpaca"
+	"net/http"
 )
 
 
 
 func main() {
-	// Get our account information.
+	getAccountInformation()
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
 
-	client := alpaca.NewClient(alpaca.ClientOpts{
-		// Alternatively you can set your key and secret using the
-		// APCA_API_KEY_ID and APCA_API_SECRET_KEY environment variables
-		APIKey:    os.Getenv("ALPACA_API_KEY"),
-		APISecret: os.Getenv("ALPACA_SECRET"),
-		BaseURL:   "https://paper-api.alpaca.markets",
-	})
-	acct, err := client.GetAccount()
+	err := http.ListenAndServe(":3333", nil)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", *acct)
-
-	// Check if our account is restricted from trading.
-	if acct.TradingBlocked {
-		fmt.Println("Account is currently restricted from trading.")
-	}
-
-	// Check how much money we can use to open new positions.
-	fmt.Printf("%v is available as buying power.\n", acct.BuyingPower)
+		
 }
--- server.go
+++ server.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 
-func server() {
-	fmt.Println("I might not make it.")
+	"github.com/alpacahq/alpaca-trade-api-go/v3/alpaca"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+func getAccountInformation() {
+	// Get our account information.
+
+	client := alpaca.NewClient(alpaca.ClientOpts{
+		// Alternatively you can set your key and secret using the
+		// APCA_API_KEY_ID and APCA_API_SECRET_KEY environment variables
+		APIKey:    os.Getenv("ALPACA_API_KEY"),
+		APISecret: os.Getenv("ALPACA_SECRET"),
+		BaseURL:   "https://paper-api.alpaca.markets",
+	})
+	acct, err := client.GetAccount()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", *acct)
+
+	// Check if our account is restricted from trading.
+	if acct.TradingBlocked {
+		fmt.Println("Account is currently restricted from trading.")
+	}
+
+	// Check how much money we can use to open new positions.
+	fmt.Printf("%v is available as buying power.\n", acct.BuyingPower)
+
 }
